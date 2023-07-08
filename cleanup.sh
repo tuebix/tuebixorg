e84631660b403ab7912dfbd5db402196db78c488
Michael Weiss <dev.primeos@gmail.com>
2023-07-09 00:03:40 +0200

    Cleanup remake.sh and cleanup.sh
    Those scripts are very old (I'm not sure if I've ever used them) and
    don't really work with the current directory structure anymore
    (especially since I've moved make.program.py back to the directory for
    2015 in 69830d2).
    The cleanup part should be integrated into the Python scripts (for 2023
    I did cleanup the files in the talks directory manually as it was only
    required 1-2 times).

--- cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rm -f _talks/*.md programm.md programm2.md programm3.md
--- remake.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-./cleanup.sh
-./make.program.py
-jekyll serve
