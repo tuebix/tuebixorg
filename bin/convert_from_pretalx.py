tuebix/tuebixorg

commit 8597e6a7ec05aaca6695cbeb24046745a93fb482

update gen scripts for new pretalx api

bin/convert_from_pretalx.py
@@ -9,6 +9,7 @@ import json
 import sys
 from datetime import datetime, timedelta
 import urllib.request
+import os
 
 
 YEAR = int(sys.argv[1] if len(sys.argv) == 2 else datetime.now().year)
@@ -23,7 +24,9 @@ with open("schedule.json", "w") as file:
 
 # TODO: Don't hardcode the year, answer IDs, etc.
 answers_url = f"https://cfp.tuebix.org/api/events/tuebix-{YEAR}/answers/?format=json&limit=100"
-with urllib.request.urlopen(answers_url) as data:
+req = urllib.request.Request(answers_url)
+req.add_header('Authorization', f'Token {os.environ["TUEBIX_PRETALX_TOKEN"]}')
+with urllib.request.urlopen(req) as data:
     all_answers = json.load(data)["results"]
 with open("answers.json", "w") as file:
     json.dump(all_answers, file)
@@ -62,7 +65,7 @@ def gen_talks():
             talk_answers = list(filter(lambda a: a["submission"] == submission, all_answers))
             pre_knowledge = ""
             if talk_answers:
-                pre_knowledge_answer = list(filter(lambda a: a["question"]["id"] == 5, talk_answers))
+                pre_knowledge_answer = list(filter(lambda a: a["question"] == 5, talk_answers))
                 if pre_knowledge_answer:
                     assert(len(pre_knowledge_answer) == 1)
                     pre_knowledge = pre_knowledge_answer[0]["answer"]

commit fd1cac2f8cbd7cf63c8e4e9b5616d1d5159c1ea8

Fix the "Vorwissen" in the schedule
This was broken since 8597e6a and we should also make sure that we only
export public questions (luckily we didn't have any real private
questions this year).

I'm not sure yet why the second `filter()` with `a["question"] == 5`
doesn't work though - that seems very strange...
Even with a quick `print()` "debugging" test it seems fine...

bin/convert_from_pretalx.py
@@ -23,7 +23,7 @@ with open("schedule.json", "w") as file:
     json.dump(schedule, file)
 
 # TODO: Don't hardcode the year, answer IDs, etc.
-answers_url = f"https://cfp.tuebix.org/api/events/tuebix-{YEAR}/answers/?format=json&limit=100"
+answers_url = f"https://cfp.tuebix.org/api/events/tuebix-{YEAR}/answers/?question=5&limit=100"
 req = urllib.request.Request(answers_url)
 req.add_header('Authorization', f'Token {os.environ["TUEBIX_PRETALX_TOKEN"]}')
 with urllib.request.urlopen(req) as data: